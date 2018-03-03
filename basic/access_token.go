b15d131005accd1682fc234aec36984281006eeb
tiger <if.true.else@gmail.com>
2018-03-03 18:20:55 +0800

    fix: reinit project structure

--- basic/access_token.go
+++ basic/access_token.go
@@ -1,18 +1,5 @@
 package basic
 
-import (
-	"github.com/gin-gonic/gin"
-	"samplewechat/library/params"
-	"fmt"
-	"log"
-)
-
-func Token(c *gin.Context) {
-	param := new (params.GetToken)
-
-	if c.Bind(param) != nil {
-		log.Println("编译出错")
-	}
-
-	fmt.Print(param)
+func GetToken() string {
+	return "token"
 }
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/TuiBianWuLu/samplewechat/basic"
+)
+
+func main() {
+
+    fmt.Println("Hell go smaple wechat")
+
+    fmt.Println("token", basic.GetToken())
+}
