2a59c3e9a920eaedf7e0019e9a24b0e1c28aef72
tiger <if.true.else@gmail.com>
2018-03-10 16:18:51 +0800

    fix: api get post common method

--- library/params/params.go
+++
@@ -1,8 +0,0 @@
-package params
-
-type (
-	GetToken struct {
-		AppId	string	`form:"app_id" json:"app_id" binding:"required"`
-		Secret	string	`form:"secret" json:"secret" binding:"required"`
-	}
-)
